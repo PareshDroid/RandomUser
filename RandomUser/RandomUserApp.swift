453a11a1d74628208871365c690b2683c3ba8461
paresh <pareshkulkarni9@gmail.com>
2025-08-17 18:01:27 +0530

    Initial Commit

---
+++ RandomUser/RandomUserApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomUserApp.swift
+//  RandomUser
+//
+//  Created by Paresh on 17/08/25.
+//
+
+import SwiftUI
+
+@main
+struct RandomUserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RandomUserTests/RandomUserTests.swift
@@ -0,0 +1,17 @@
+//
+//  RandomUserTests.swift
+//  RandomUserTests
+//
+//  Created by Paresh on 17/08/25.
+//
+
+import Testing
+@testable import RandomUser
+
+struct RandomUserTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
