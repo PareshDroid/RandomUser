9908ce35d56aa95447802b405d45f98b981dd12e
Paresh <pareshkulkarni9@gmail.com>
2025-08-20 10:54:05 +0530

    comments added in view model for better understanding of code

--- RandomUser/ViewModel/UserViewModel.swift
+++ RandomUser/ViewModel/UserViewModel.swift
@@ -15,8 +15,14 @@ class UserViewModel: ObservableObject {
     private(set) var hasLoaded = false
 
     func fetchUsers() {
+        //guard is a control flow statement used to early exit a scope if certain conditions aren't met
+        
         guard !hasLoaded else { return }
         fetchUsersFromNetwork()
+        
+        //If hasLoaded is **true**, it means data has already been loaded → so we exit early and skip the network call.
+
+        //If hasLoaded is **false**, it means data hasn’t been loaded yet → continue and call fetchUsersFromNetwork().
     }
 
     func forceReload() {
@@ -35,24 +41,24 @@ class UserViewModel: ObservableObject {
         }
 
         URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
-            DispatchQueue.main.async {
-                self?.isLoading = false
+            DispatchQueue.main.async { // Ensures UI is updated on main thread
+                self?.isLoading = false //loading has finished so marking the loading as false
 
                 if let error = error {
-                    self?.errorMessage = error.localizedDescription
+                    self?.errorMessage = error.localizedDescription //if network error then set the error message and exit early
                     return
                 }
 
                 guard let data = data else {
-                    self?.errorMessage = "No data"
+                    self?.errorMessage = "No data" // if no data came back (though no error occurred), set a different error message and exit early.
                     return
                 }
 
                 do {
-                    let decoded = try JSONDecoder().decode(RandomUserResponse.self, from: data)
+                    let decoded = try JSONDecoder().decode(RandomUserResponse.self, from: data) //parse data using the defined data model
                     self?.users = decoded.results
                 } catch {
-                    self?.errorMessage = "Decoding error: \(error.localizedDescription)"
+                    self?.errorMessage = "Decoding error: \(error.localizedDescription)" //if decoding fails set the respective error message
                 }
             }
         }.resume()
